5d1ec9e90dd2458fa9a2f5c8bfee75d26c55b446
Abdullah Alhaider <>
2018-08-03 06:49:33 +0300

    Creating profile and other enhancement

--- Hackthon/Hajj/Profile/ProfileViewController.swift
+++ Hackthon/Hajj/Profile/ProfileViewController.swift
@@ -19,26 +19,63 @@ class ProfileViewController: UIViewController {
     @IBOutlet weak var view1: UIView!
     @IBOutlet weak var view2: UIView!
     
+    @IBOutlet weak var view3: UIView!
+    @IBOutlet weak var view4: UIView!
+    
+    @IBOutlet weak var view5: UIView!
+    @IBOutlet weak var view6: UIView!
     
     @IBOutlet weak var exitButton: UIButton!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setup()
         // Do any additional setup after loading the view.
     }
     
     
     @IBAction func exitButtonPresed(_ sender: UIButton) {
-        dismiss(animated: true, completion: nil)
+       dismiss()
+    }
+    
+    fileprivate func dismiss(){
+        dismiss(animated: true) {
+            NotificationCenter.default.post(name: NSNotification.Name(rawValue: "showBackgroundView"), object: nil)
+        }
     }
     
     fileprivate func setup() {
+        view.backgroundColor = .clear
+        backView.applyViewDesign(masksToBounds: false, shadowColor: .darkGray, cornerRadius: 10, shadowOpacity: 0.4, shadowOffset: CGSize(width: 0, height: 0), shadowRadius: 12)
         
-        backView.applyViewDesign(masksToBounds: false, shadowColor: .darkGray, cornerRadius: 10, shadowOpacity: 0.2, shadowOffset: CGSize(width: 0, height: 0), shadowRadius: 9)
+        profileImage.image = "me".asImage
+        profileImage.clipsToBounds = true
+        profileImage.layer.cornerRadius = profileImage.frame.size.height / 2
+        profileImage.layer.borderWidth = 2.5
+        profileImage.layer.borderColor = UIColor.grayColor.cgColor
         
+        view1.layer.cornerRadius = 5
+        view1.backgroundColor = .grayColor
         
+        view2.layer.cornerRadius = 5
+        view2.backgroundColor = .grayColor
+        
+        view3.layer.cornerRadius = 5
+        view3.backgroundColor = .grayColor
+        
+        view4.layer.cornerRadius = 5
+        view4.backgroundColor = .grayColor
+        
+        view5.layer.cornerRadius = 5
+        view5.backgroundColor = .grayColor
+        
+        view6.layer.cornerRadius = 5
+        view6.backgroundColor = .grayColor
+        
+        exitButton.layer.cornerRadius = 5
+        exitButton.layer.borderWidth = 1
+        exitButton.layer.borderColor = #colorLiteral(red: 0.3333333433, green: 0.3333333433, blue: 0.3333333433, alpha: 1)
     }
     
     
